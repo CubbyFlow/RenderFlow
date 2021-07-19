da919f78d421fe620e55ecaef578bbb07a11c387
snowapril <sinjihng@gmail.com>
2021-07-19 20:36:53 +0900

    feat(remove-glm): implement view & projection transform with CubbyFlow (WIP)

--- Includes/Core/MathUtils.hpp
+++ Includes/Core/MathUtils.hpp
@@ -1,6 +1,7 @@
 #ifndef MATHUTILS_HPP
 #define MATHUTILS_HPP
 
+#include <Matrix/Matrix.hpp>
 #include <functional>
 
 namespace Core
@@ -20,6 +21,36 @@ template <typename Type>
 Type CubicSpline(Type prev, Type next, const float keyframe);
 
 template <typename Type>
+namespace Transform
+{
+/**
+ * @brief
+ *
+ * @tparam Type
+ * @param origin
+ * @param direction
+ * @return CubbyFlow::Matrix4x4<Type>
+ * @param up
+ */
+template <typename Type>
+CubbyFlow::Matrix4x4<Type> LookAt(CubbyFlow::Vector3<Type> origin,
+                                  CubbyFlow::Vector3<Type> direction,
+                                  CubbyFlow::Vector3<Type> up);
+
+/**
+ * @brief
+ *
+ * @tparam Type
+ * @param radian
+ * @param aspectRatio
+ * @param zNear
+}  // namespace Transform
+CubbyFlow::Matrix4x4<Type> Perspective(Type radian, Type aspectRatio,
+                                       Type zNear, Type zFar);
+template <typename Type>
+ */
+ * @return CubbyFlow::Matrix4x4<Type>
+ * @param zFar
 Type Step(Type prev, Type next, const float keyframe);
 };  // namespace Interpolation
 };  // namespace Core
