cbb8e35c45911d8eb0df795ecedc6aaf6cdc5a68
snowapril <sinjihng@gmail.com>
2021-07-20 13:28:00 +0900

    feat : add converting cubbyflow generated files to gltf project

---
+++ Extensions/Fluid2GLTF/main.cpp
@@ -0,0 +1,20 @@
+#include <cxxopts/cxxopts.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	cxxopts::Options options(argv[0], "simple description");
+
+	options.add_options()
+		("h,help", "Print usage");
+
+	auto result = options.parse(argc, argv);
+
+	if (result.count("help"))
+	{
+		std::cout << options.help() << std::endl;
+		exit(0);
+	}
+
+	return 0;
+}
